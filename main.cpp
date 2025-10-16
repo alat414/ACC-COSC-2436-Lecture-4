fab34289723d7d81907bce3be18c15628d1d0f30
alat414 <gustavo.alatriste@g.austincc.edu>
2025-10-15 23:38:37 -0500

    adding class and vector templates

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-ECHO is on.
+#include <iostream>
+#include <vector>
+#include <list>
+#include <fstream>
+
+std::vector<std::string> loadCityNames(const char* city_list_name)
+{
+    std::ifstream fill_object(city_list_name);
+}
+
+class flightMap
+{
+    private:
+        std::vector<std::string> cityNames;
+    public:
+        flightMap(const char* city_list_name, const char* flight_routes_name)
+        {
+
+        }
+};
+
+int main()
+{
+
+}
